527173db05b7f1839e439e1833ed831b83cfc5c3
hitomi.sasaki <tommy110327@yahoo.co.jp>
2021-01-04 09:48:19 +0000

    add status column to tasklist

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -12,6 +12,7 @@
             <tr>
                 <th>id</th>
                 <th>タスク</th>
+                <th>ステータス</th>
             </tr>
         </thead>
         <tbody>
@@ -20,6 +21,7 @@
                 {{-- タスク詳細ページへのリンク　--}}
                 <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                 <td>{{ $task->content }}</td>
+                <td>{{ $task->status }}</td>
             </tr>
             @endforeach
         </tbody>
